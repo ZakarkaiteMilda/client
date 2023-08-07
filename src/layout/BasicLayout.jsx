e9aeb09a9bd2298b91ee626706a75ccb0fff6da4
ZakarkaiteMilda <zakarkaite.milda@gmail.com>
2023-08-08 04:02:12 +1400

    initial commit

---
+++ src/layout/BasicLayout.jsx
@@ -0,0 +1,11 @@
+// import style from './BasicLayout.module.css';
+
+	export function BasicLayout() {
+    		return (
+        		<>
+          		 <header></header>
+                 <main></main>
+                 <footer></footer>
+        		</>
+  		  );
+	}

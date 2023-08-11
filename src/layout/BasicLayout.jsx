388c94daa6c1a21a27bd344370c5af898d285067
ZakarkaiteMilda <zakarkaite.milda@gmail.com>
2023-08-12 02:06:06 +1400

    commit

--- src/layout/BasicLayout.jsx
+++ src/layout/BasicLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 // import style from './BasicLayout.module.css';
+import { Header } from '../components/Header.jsx';
+import { Footer } from '../components/Footer.jsx';
 
 	export function BasicLayout() {
     		return (
         		<>
-          		 <header>HEADER</header>
+          		 < Header />
                  <main>< Outlet /></main>
-                 <footer>FOOTER</footer>
+				 < Footer />
         		</>
   		  );
 	}
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,8 +2,19 @@
 
 export function NoPage() {
         return (
-            <div>
-               404 CONTENT
+            <div className='container'>
+            <div className='row'>
+                <div className='col-12'>12</div>
             </div>
+            <div className='row'>
+                <div className='col-6'>6</div>
+                <div className='col-6'>6</div>
+            </div>
+            <div className='row'>
+                <div className='col-4'>4</div>
+                <div className='col-4'>4</div>
+                <div className='col-4'>4</div>
+            </div>
+         </div>
         );
 }
